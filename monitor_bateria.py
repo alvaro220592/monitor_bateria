ffc83dff23c3606a79488e61b773be5ef2c7616d
alvaro220592 <alvaro220592@gmail.com>
2022-12-09 23:12:49 -0300

    comentários

--- monitor_bateria.py
+++ monitor_bateria.py
@@ -1,23 +1,28 @@
 ## Este script é executado ao iniciar o sistema
 ## O comando está em .config/autostart/monitor_bateria.desktop
 
+# certifique-se que tenha instalados esses módulos. Caso não tenha, execute no terminal: pip install nomeDoMóduloDesejado
 import psutil
 from plyer import notification
 import time
 import playsound
 
+# executará infinitamente
 while(True):
-	bateria = psutil.sensors_battery()
-	nivel = bateria.percent
-	carregando = bateria.power_plugged
+	bateria = psutil.sensors_battery() # definindo a bateria
+	nivel = bateria.percent # porcentagem da bateria
+	carregando = bateria.power_plugged # se está carregando(true ou false)
 
+	# Se estiver com menos de 15 e 'carregando' for false
 	if nivel < 15 and not carregando:
+		# chamando a notificação
 		notification.notify(
 			title = "Nível de bateria",
-			message = (f'{nivel:,.0f}% restantes\nCarregue a bateria!'),
-			timeout = 10
+			message = (f'{nivel:,.0f}% restantes\nCarregue a bateria!'), # mostrando a mensagem com o nível de bateria sem casas decimais
+			timeout = 10 # tempo de exibição da notificação
 		)
 
+		# tocando o som(opcional)
 		playsound.playsound('/Endereço/completo/do/seu/mp3.mp3')
 
 		time.sleep(10)
